2364f07281a5e89fdafcf318e995641c32c2739c
camilozv21 <camilozv21@gmail.com>
2023-08-14 16:16:48 -0500

    finalizacion

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import '../assets/css/app.css';
 import SideBar from './SideBar';
+import ContentWrapper from './ContentWrapper';
 
 function App() {
   return (
     <div className="App">
-      <SideBar />
+      <div id="wrapper">
+        <SideBar />
+        <ContentWrapper />
+      </div>
     </div>
   );
 }
